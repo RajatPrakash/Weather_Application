f57995e6d5cbe72d853eca2428570c77b37a9711
Rajat Prakash <rajatprakash904@gmail.com>
2019-11-29 09:40:47 +0530

    Name Updated

--- Weather_application.py
+++ Weather_application.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import request
-import json
+from flask import jsonify
 # from pip._vendor import requests
 
 window = tk.Tk()
@@ -15,23 +15,22 @@ image_label = tk.Label(window,image=render)
 image_label.place(relheight=1,relwidth=1)
 
 
-def get_weather(entry):
+def get_weather():
     print('this is the weather')
-    # key = '6557bbd7dbca062ddf4fe097dc1XXXX'
-    # #  api.openweathermap.org/data/2.5/weather?q={city name},{country code}
-    # url = 'https://api.openweathermap.org/data/2.5/weather'
-    # APPID = key
-    # q = entry
-    # units'metric'}
-    # response = request.GET(url, params={'APPID': key,'q': entry,'units':'metric'})
-    # print(response.json())
+    user = entry.get()
+    key = '6557bbd7dbca062ddf4fe097XXXX'
+    # api.openweathermap.org/data/2.5/weather?q={city name},{country code}
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID':key,'q': user,'units':'metric'}
+    response = request.GET(url, params=params)
+    return jsonify(response.JSON())
 
 
 top_frame = tk.Frame(window)
 top_frame.place(relx=0.1,rely=0.2,relwidth=0.55,relheight=0.07)
 entry = tk.Entry(top_frame,bg='Grey', borderwidth=5, font = ('Arial BOLD', 20))
 entry.place(relheight=1,relwidth=1)
-button = tk.Button(window,text="Get Weather!",bg='black',fg='white', font=('Arial BOLD', 18),command=lambda:(get_weather(entry.get())) )
+button = tk.Button(window,text="Get Weather!",bg='black',fg='white', font=('Arial BOLD', 18,), command= get_weather)
 button.place(relx=0.7,rely=0.2,relwidth=0.25,relheight=0.07)
 
 bottom_frame = tk.Frame(window,bg='grey')
